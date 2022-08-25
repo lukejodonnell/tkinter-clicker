be43311dc01008e5388d7c1b739f179d9f6e15ba
luke <lukejodonnell@gmail.com>
2022-08-24 20:09:23 -0400

    Might be my last commit to main for a while, my goal was to make a starting point

--- clicker.py
+++ clicker.py
@@ -1,4 +1,5 @@
-
+#We will use tkinter later, right now it's important to make sure we can import it
+import tkinter
 
 # This if statment just says "if this python script is the 'main' one, do this"
 if __name__ == "__main__":
